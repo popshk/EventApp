efff15dc570c391f0fcd787898fb1aa2a1883039
YeninMy <vbif555666@gmail.com>
2023-11-02 20:21:41 +0200

    DataLoader DataCleaner

--- src/main/java/com/event/app/service/EventService.java
+++ src/main/java/com/event/app/service/EventService.java
@@ -1,8 +1,6 @@
 package com.event.app.service;
 
-import com.event.app.model.Event;
 import com.event.app.repo.EventRepo;
-import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,15 +9,4 @@ public class EventService {
 
     @Autowired
     private EventRepo repository;
-
-    @PostConstruct
-    private void init() {
-        repository.insert(Event.builder().name("jopa").build());
-    }
-
-    @PostConstruct
-    private void find() {
-        System.out.println(repository.findById("65426c8dc723952288906b11").get());
-    }
-
 }
