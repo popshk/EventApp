bf33082cba2286559e591d14ca3ba297d2c72d17
YeninMy <vbif555666@gmail.com>
2023-11-06 00:58:39 +0200

    method get all events

--- src/main/java/com/event/app/config/DataLoader.java
+++ src/main/java/com/event/app/config/DataLoader.java
@@ -1,17 +1,20 @@
 package com.event.app.config;
+
 import com.event.app.model.Customer;
 import com.event.app.model.Event;
 import com.event.app.repo.CustomerRepo;
 import com.event.app.repo.EventRepo;
+import com.event.app.service.CustomerService;
+import com.event.app.service.EventService;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-
 public class DataLoader {
     private final CustomerRepo customerRepository;
     private final EventRepo eventRepository;
+
 
     @Autowired
     public DataLoader(CustomerRepo customerRepository, EventRepo eventRepository) {
@@ -23,6 +26,8 @@ public class DataLoader {
     @PostConstruct
     private void init() {
         customerRepository.insert(Customer.builder().nickName("Vadya").build());
-        eventRepository.insert(Event.builder().name("jopa").build());
+        eventRepository.insert(Event.builder().name("jopa").creator(Customer.builder().nickName("Vadya2").build()).build());
+        eventRepository.insert(Event.builder().name("jopa2").build());
     }
+
 }
--- src/main/java/com/event/app/service/EventService.java
+++ src/main/java/com/event/app/service/EventService.java
@@ -1,12 +1,30 @@
 package com.event.app.service;
 
+import com.event.app.model.Customer;
+import com.event.app.model.Event;
 import com.event.app.repo.EventRepo;
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Service
 public class EventService {
 
     @Autowired
-    private EventRepo repository;
+    private EventRepo eventRepo;
+
+    @PostConstruct
+    public Set<Event> getAllEvents() {
+        Set<Event> allEvents = new HashSet<>(eventRepo.findAll());
+        System.out.println(allEvents);
+        return allEvents;
+    }
+
+    public Set<Event> getAllEventsByCreator(Customer creator) {
+        Set<Event> allEventsByCreator = new HashSet<>(eventRepo.getEventsByCreator(creator));
+        return allEventsByCreator;
+    }
 }
